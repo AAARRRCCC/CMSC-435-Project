b18a12f29b42d63183eefcfc770710d4ba56e036
Ryan Brady <rbrady.business@gmail.com>
2025-11-04 18:33:20 -0500

    reordered output in calculatiemetrics

--- Python-Scripts/predictions_to_confusion.py
+++ Python-Scripts/predictions_to_confusion.py
@@ -2,12 +2,13 @@ import pandas as pd
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
 
-def getConfusionDF():
+def getConfusionDF(filepath):
 
-    df = pd.read_csv('Dataset/rapidminer_results.csv')
+    df = pd.read_csv(filepath)
     y_true = df['class']
     y_pred = df['prediction(class)']
 
     cm = confusion_matrix(y_pred, y_true, labels=['nonDRNA','RNA', 'DNA', 'DRNA'])
     cm_df = pd.DataFrame(cm, index=['nonDRNA','RNA', 'DNA', 'DRNA'], columns=['nonDRNA','RNA', 'DNA', 'DRNA'])
     #print(cm_df)
+    return cm_df
